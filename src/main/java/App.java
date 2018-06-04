58c5ebdbfda5e795e2a910524ee04238295778de
martastaporek <staporekmarta1@gmail.com>
2018-06-04 15:17:34 +0200

    Implement count bombs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,8 @@ public class App {
     public static void main(String[] args) {
         Minesweeper minesweeper = new Minesweeper();
         char [] [] dotes = minesweeper.generateMap(5);
-        minesweeper.printDotes(dotes);
+        minesweeper.printMap(dotes);
+        int [] [] numbers = minesweeper.generateNumbers(dotes);
+        minesweeper.printMap(numbers);
     }
 }
--- src/main/java/Minesweeper.java
+++ src/main/java/Minesweeper.java
@@ -2,11 +2,11 @@ import java.util.Random;
 
 public class Minesweeper {
 
-    private final char [] charArray = {'.', '.', '*'};
+    private final char [] charArray = {'.', '.', '*', '.'};
 
     public char [] [] generateMap(int rowNumber){
 
-        char [] [] generatedMap = new char[rowNumber][rowNumber];
+        char [] [] generatedMap = new char [rowNumber][rowNumber];
 
         for(int i = 0; i < rowNumber; i++){
             for(int j = 0; j < rowNumber; j++){
@@ -18,12 +18,71 @@ public class Minesweeper {
         return generatedMap;
     }
 
-    public void printDotes(char [] [] array){
+    public void printMap(char [] [] array){
         for (int i = 0; i < array.length; i++){
             for (int j = 0; j< array.length; j++){
                 System.out.print(array[i][j]);
             }
             System.out.println(" ");
         }
+    }
+
+    public void printMap(int [] [] array){
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j< array.length; j++){
+                System.out.print(array[i][j]);
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public int [] [] generateNumbers(char [] [] dotesMap) {
+
+        int[][] numberMap = new int[dotesMap.length][dotesMap.length];
+
+        for (int i = 0; i < dotesMap.length; i++) {
+            for (int j = 0; j < dotesMap.length; j++) {
+                int counter = 0;
+                if(dotesMap[i][j] == '*'){
+                    counter++;
+                }
+                try {
+                    if (dotesMap[i][j - 1] == '*') {
+                        counter++;
+                    }
+                } catch (IndexOutOfBoundsException e) {
+
+                } finally {
+                    try {
+                        if (dotesMap[i][j + 1] == '*') {
+                            counter++;
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+
+                    } finally {
+                        try {
+                            if (dotesMap[i - 1][j] == '*') {
+                                counter++;
+                            }
+                        } catch (IndexOutOfBoundsException e) {
+
+                        } finally {
+                            try {
+                                if (dotesMap[i + 1][j] == '*') {
+                                    counter++;
+                                }
+                            } catch (IndexOutOfBoundsException e) {
+
+                            }
+                        }
+                    }
+
+                }
+                numberMap[i][j] = counter;
+
+            }
+        }
+        return numberMap;
+
     }
 }
